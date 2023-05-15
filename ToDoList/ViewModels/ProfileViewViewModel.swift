0303a3c89b72af8ccc2e461c6c9682f4dd389e2a
KaarthikeyaK <kaarthu2003@gmail.com>
2023-05-15 21:46:43 +0530

    Checking ToDo Items

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by KAARTHIKEYA K on 03/05/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class profileViewViewModel: ObservableObject{
@@ -13,6 +15,17 @@ class profileViewViewModel: ObservableObject{
     }
     
     func toggleIsDone(item: ToDoListItem){
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
