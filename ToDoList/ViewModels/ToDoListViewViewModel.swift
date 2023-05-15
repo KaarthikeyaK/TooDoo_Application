b768e6fd6d87278a3dacbede5c5369b73144b56e
KaarthikeyaK <kaarthu2003@gmail.com>
2023-05-15 21:37:36 +0530

    Deleting To Do List Items.

--- ToDoList/ViewModels/ToDoListViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -5,18 +5,30 @@
 //  Created by KAARTHIKEYA K on 03/05/23.
 //
 
+import FirebaseFirestore
 import Foundation
 
 // ViewModel for the list of items view
 class ToDoListViewViewModel : ObservableObject {
     @Published var showingNewItemView = false
     
-    init(){
-        
+    private let userId: String
+    
+    init(userId: String){
+        self.userId = userId
     }
     
+    
+    /// Delete To Do list item
+    /// - Parameter id: Item Id to delte 
     func delete(id: String){
+        let db = Firestore.firestore()
         
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
     }
 
 }
